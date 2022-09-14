dc532c24742bf58b92f72d2e6c66c3fc31880ba1
Егор <egor.zholobov@yandex.ru>
2022-09-14 12:40:50 +0300

    Add project files.

---
+++ EventMaker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventMaker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
